fbb7ab2f666ad1808758ce1f791b6ae7f2f7d040
Stepan Karpov <stepan-karpov-2025@mail.ru>
2026-01-14 16:32:52 +0300

    create db_helper

---
+++ app/core/database/db_helper.py
@@ -0,0 +1,12 @@
+from core.database.base import BaseDatabaseHelper
+
+from core.config import settings
+
+
+db_helper = BaseDatabaseHelper(
+    url=str(settings.db.url),
+    echo=settings.db.echo,
+    echo_pool=settings.db.echo_pool,
+    max_overflow=settings.db.max_overflow,
+    pool_size=settings.db.pool_size,
+)
