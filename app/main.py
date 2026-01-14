96176d9a90d0fcc3eaac3c3f130adf5ee5889051
Stepan Karpov <stepan-karpov-2025@mail.ru>
2026-01-14 16:50:29 +0300

    create main.py for start app

---
+++ app/main.py
@@ -0,0 +1,40 @@
+from contextlib import asynccontextmanager
+
+import uvicorn
+
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+
+from api.middlewares.register_middleware import register_middleware
+
+from core.config import settings
+
+from api import router as api_router
+
+from core.database import db_helper
+
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+    await db_helper.dispose()
+
+
+main_app = FastAPI(
+    default_response_class=ORJSONResponse,
+    lifespan=lifespan,
+)
+
+
+main_app.include_router(api_router, prefix=settings.api.prefix)
+register_middleware(main_app)
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:main_app",
+        host=settings.run.host,
+        port=settings.run.port,
+    )
